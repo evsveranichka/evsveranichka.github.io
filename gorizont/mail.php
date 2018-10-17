282b5adee77e41621372c455f25c123a9c2b1215
evsveranichka <37903509+evsveranichka@users.noreply.github.com>
2018-10-17 13:03:15 +0300

    Add files via upload

--- gorizont/mail.php
+++ gorizont/mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "evsveranichka@gmail.com";
+        $recipient = "info@горизонт.бел";
 
         // Set the email subject.
         $subject = "New contact from $name";
